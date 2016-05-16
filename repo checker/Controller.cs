ecfa8ff745c6a98411a186b1ad78181df92a45a8
Vasiliy Galeev <vasya.galeev@gmail.com>
2016-05-16 13:20:18 +0300

    Add controller and some functions for GitHub user

---
+++ repo checker/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Octokit;
+using Octokit.Helpers;
+using Octokit.Internal;
+
+namespace repo_checker
+{
+    class Controller
+    {
+        private Form1 _view;
+
+        public Controller(Form1 view)
+        {
+            this._view = view;
+            this._view.PrintRepositoriesByUser += GetRepositoriesByUser;
+        }
+
+        private async void GetRepositoriesByUser()
+        {
+            var ghe = new Uri("https://github.com/");
+            var client = new GitHubClient(new ProductHeaderValue(_view.GetUsername()), ghe);
+            var test = await client.Repository.Content.GetAllContents(_view.GetUsername(), ghe);
+
+            var repos = await client.Repository.GetAllForUser(_view.GetUsername());
+
+            //DateTimeOffset data;
+            foreach (var repository in repos)
+            {
+                _view.GetUserRepositoriesListBox().Items.Add(repository.Name);
+            }
+        }
+    }
+}
+
---
+++ repo checker/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Octokit;
+using Octokit.Helpers;
+using Octokit.Internal;
+
+namespace repo_checker
+{
+    public partial class Form1 : Form
+    {
+        public delegate void UserEventHandler();
+
+        public event UserEventHandler PrintRepositoriesByUser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetUsername()
+        {
+            return this.usernameTextBox.Text.ToString();
+        }
+
+        public ListBox GetUserRepositoriesListBox()
+        {
+            return this.usersRepositoriesListBox;
+        }
+
+        private void showRepositoriesButton_Click(object sender, EventArgs e)
+        {
+            PrintRepositoriesByUser();
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
